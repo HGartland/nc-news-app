c7ce5ead90c9f56af58f52d1221a2b3afbb4a535
HGartland <harlenski@hotmail.co.uk>
2019-12-18 12:09:57 +0000

    changed CommentForm to use required for feedback on an empty comment

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -13,7 +13,7 @@ class CommentForm extends Component {
       body: this.state.comment
     };
     if (/^\s+$/.test(this.state.comment)) alert("comments must not be empty");
-    else if (this.state.comment.length > 0) {
+    else {
       this.setState({ paused: true });
       api
         .postComment(this.props.article_id, newComment)
@@ -35,16 +35,15 @@ class CommentForm extends Component {
 
   render() {
     return (
-      <form className="Comment-form">
+      <form className="Comment-form" onSubmit={this.handleSubmit}>
         <h5>New Comment:</h5>
         <textarea
           onChange={this.handleChange}
           value={this.state.comment}
           cols={190}
+          required
         ></textarea>
-        <button onClick={this.handleSubmit} disabled={this.state.paused}>
-          Submit
-        </button>
+        <button disabled={this.state.paused}>Submit</button>
       </form>
     );
   }
